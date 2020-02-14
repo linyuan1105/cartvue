df00794efbe71f57ca863a5b2f48165ea8264e8d
linyuan1105 <673609957@qq.com>
2020-02-14 16:57:18 +0800

    1

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -6,37 +6,28 @@ const webpackBaseConfig = require('./webpack.base.config.js');
 const fs = require('fs');
 
 fs.open('./src/config/env.js', 'w', function (err, fd) {
-    const buf = 'export default "production";';
-    fs.write(fd, buf, 0, buf.length, 0, function (err, written, buffer){});
+    const buf = 'export default "development";';
+    fs.write(fd, buf, 0, 'utf-8', function(err, written, buffer) {});
 });
 
 module.exports = merge(webpackBaseConfig, {
+    devtool: '#source-map',
     output: {
         publicPath: '/dist/',
-        filename: '[name].[hash].js',
-        chunkFilename: '[name].[hash].chunk.js'
+        filename: '[name].js',
+        chunkFilename: '[name].chunk.js'
     },
     plugins: [
         new ExtractTextPlugin({
-            filename: '[name].[hash].css',
+            filename: '[name].css',
             allChunks: true
         }),
         new webpack.optimize.CommonsChunkPlugin({
             name: 'vendors',
-            filename: 'vendors.[hash].js'
-        }),
-        new webpack.DefinePlugin({
-            'process.env': {
-                NODE_ENV: '"production"'
-            }
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings: false
-            }
+            filename: 'vendors.js'
         }),
         new HtmlWebpackPlugin({
-            filename: '../index_prod.html',
+            filename: '../index.html',
             template: './src/template/index.ejs',
             inject: false
         })
